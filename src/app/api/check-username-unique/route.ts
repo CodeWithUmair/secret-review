8473ec06324829cedfea56ac8019c99e9f0e7145
umairamir007 <umairamir@decryptedlabs.io>
2024-08-22 07:39:34 +0500

    .

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,49 @@
+import UserModel from "@/model/User";
+import dbConnect from "@/lib/dbConnect";
+import { z } from "zod";
+import { usernnameValidation } from "@/schema/signUpSchema";
+
+const UsernameQuerySchema = z.object({
+  username: usernnameValidation,
+});
+
+export async function GET(req: Request) {
+  await dbConnect();
+
+  try {
+    const { searchParams } = new URL(req.url);
+    const queryParam = {
+      username: searchParams.get("username"),
+    };
+
+    // validation with zod
+    const result = UsernameQuerySchema.safeParse(queryParam);
+
+    if (!result.success) {
+      const usernameErrors = result.error.format().username?._errors || [];
+      return Response.json(
+        {
+          success: false,
+          message:
+            usernameErrors?.length > 0
+              ? usernameErrors.join(", ")
+              : "Invalid query parameter",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+  } catch (error) {
+    console.error("Error checking username", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error checking username",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
